0a2bae06c2a02dfeb3e3361e3a5684133af9dccc
Влад Дьяконов <vladivc@iatu.local>
2020-03-13 15:48:56 +0400

    Защита процесса

--- Iatu Info/Iatu Info/Form1.Designer.cs
+++ Iatu Info/Iatu Info/Form1.Designer.cs
@@ -29,12 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(984, 761);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://vk.com/theyhd", System.UriKind.Absolute);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(378, 726);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Система информирования";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Iatu Info/Iatu Info/Form1.cs
+++ Iatu Info/Iatu Info/Form1.cs
@@ -7,14 +7,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Diagnostics;
 namespace Iatu_Info
 {
     public partial class Form1 : Form
     {
+        public bool close_it = false;
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TopMost = true;
+            ProtectionProcess pp = new ProtectionProcess();
+            pp.ProtectionOn(Process.GetCurrentProcess().Handle);
+            Console.Write(pp);
+            Console.Write(Process.GetCurrentProcess().Handle);
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!close_it)
+            {
+                e.Cancel = true;
+                base.OnClosing(e);
+                this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        int numb = 10;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.Write(e);
+            if (numb == 0)
+            {
+                button1.Enabled = true;
+                button1.Text = "Закрыть";
+            }
+            else
+            {
+                numb--;
+                button1.Text = "Можно закрыть через "+ numb;
+            }
         }
     }
 }
+
+     class ProtectionProcess
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool GetKernelObjectSecurity(IntPtr Handle, int securityInformation, [Out] byte[] pSecurityDescriptor,
+        uint nLength, out uint lpnLengthNeeded);
+
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool SetKernelObjectSecurity(IntPtr Handle, int securityInformation, [In] byte[] pSecurityDescriptor);
+
+        [Flags]
+        private enum ProcessAccessRights
+        {
+            PROCESS_CREATE_PROCESS = 0x0080,
+            PROCESS_CREATE_THREAD = 0x0002,
+            PROCESS_DUP_HANDLE = 0x0040,
+            PROCESS_QUERY_INFORMATION = 0x0400,
+            PROCESS_QUERY_LIMITED_INFORMATION = 0x1000,
+            PROCESS_SET_INFORMATION = 0x0200,
+            PROCESS_SET_QUOTA = 0x0100,
+            PROCESS_SUSPEND_RESUME = 0x0800,
+            PROCESS_TERMINATE = 0x0001,
+            PROCESS_VM_OPERATION = 0x0008,
+            PROCESS_VM_READ = 0x0010,
+            PROCESS_VM_WRITE = 0x0020,
+            DELETE = 0x00010000,
+            READ_CONTROL = 0x00020000,
+            SYNCHRONIZE = 0x00100000,
+            WRITE_DAC = 0x00040000,
+            WRITE_OWNER = 0x00080000,
+            STANDARD_RIGHTS_REQUIRED = 0x000f0000,
+            PROCESS_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0xFFF),
+        }
+
+        private void SetProcessSecurityDescriptor(IntPtr processHandle, RawSecurityDescriptor dacl)
+        {
+            const int DACL_SECURITY_INFORMATION = 0x00000004;
+            byte[] rawsd = new byte[dacl.BinaryLength];
+            dacl.GetBinaryForm(rawsd, 0);
+            if (!SetKernelObjectSecurity(processHandle, DACL_SECURITY_INFORMATION, rawsd))
+                throw new Win32Exception();
+        }
+
+        private RawSecurityDescriptor GetProcessSecurityDescriptor(IntPtr processHandle)
+        {
+            const int DACL_SECURITY_INFORMATION = 0x00000004;
+            byte[] psd = new byte[0];
+            uint bufSizeNeeded;
+            GetKernelObjectSecurity(processHandle, DACL_SECURITY_INFORMATION, psd, 0, out bufSizeNeeded);
+            if (bufSizeNeeded < 0 || bufSizeNeeded > short.MaxValue)
+                throw new Win32Exception();
+            if (!GetKernelObjectSecurity(processHandle, DACL_SECURITY_INFORMATION,
+            psd = new byte[bufSizeNeeded], bufSizeNeeded, out bufSizeNeeded))
+                throw new Win32Exception();
+            return new RawSecurityDescriptor(psd, 0);
+        }
+        public void ProtectionOn(IntPtr hProcess)
+        {
+            var dacl = GetProcessSecurityDescriptor(hProcess);
+            dacl.DiscretionaryAcl.InsertAce(0, new CommonAce(AceFlags.None, AceQualifier.AccessDenied, (int)ProcessAccessRights.PROCESS_ALL_ACCESS, new SecurityIdentifier(WellKnownSidType.WorldSid, null), false, null));
+            SetProcessSecurityDescriptor(hProcess, dacl);
+        }
+    }
